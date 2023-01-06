c4053ec0a679cf2eb916376cfa5fc6d129f73fbc
n0thing289 <61171141+n0thing289@users.noreply.github.com>
2023-01-06 21:34:53 +0800

    super

---
+++ super/src/super_/A.java
@@ -0,0 +1,42 @@
+package super_;
+
+public class A {
+    public int n1 = 100;
+    protected int n2 = 200;
+    int n3 = 300;
+    private int n4 = 400;
+
+    public A() {
+        //这个默认无参构造器可以不写，但是一旦你自定义重载了构造器了就最好显式写
+        // （因为当你自己定义了一个构造器的时候，
+        // 默认的无参构造器会被覆盖-->显示定义一个无参构造器，构成构造器重载）
+    }
+
+    public A(String name) {
+
+    }
+
+    public A(String name, int age) {
+
+    }
+
+    public void test100() {
+    }
+
+    protected void test200() {
+
+    }
+
+    void test300() {
+
+    }
+
+    private void test400() {
+
+    }
+
+    public void cal(){
+        System.out.println("A类的cal()方法被调用...");
+    }
+
+}
---
+++ super/src/super_/B.java
@@ -0,0 +1,41 @@
+package super_;
+
+public class B extends A {
+    //一旦用了继承，首先考虑有无指定调用哪一个的父类的构造器
+    //考虑包括：默认的和自定义重载的
+    public int n1 = 888;
+    public void hi() {
+//        System.out.println(super.n1 + " " + super.n2 + " " + super.n3);
+    }
+    public void ok(){
+        super.test100();
+        super.test200();
+        super.test300();
+//        super.test400();//私有不能直接这样访问
+    }
+
+
+    //自定义构造器
+    public B(){
+//        super();//显式地指定调用父类的无参构造器//可以不写，也就是默认调用无参
+//        super("jack");//当父类有多个构造器时
+        super("jack", 18);
+    }
+
+    public void sum(){
+        System.out.println("B类的sum()方法...");
+//        cal();//注释A中cal()和private void cal()提示信息不一样
+//        this.cal();
+        this.cal();//直接从父类开始找,其余逻辑一样
+        super.cal();
+
+        //检验属性的向上查找机制
+//        System.out.println(n1);
+//        System.out.println(this.n1);
+        System.out.println(super.n1);
+    }
+    public void cal(){
+        System.out.println("B类的cal()方法");
+    }
+
+}
