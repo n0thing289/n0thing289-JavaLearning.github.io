d55a6ec0e02fa56e4bfc2e22ff87b14a09c0ae72
n0thing289 <61171141+n0thing289@users.noreply.github.com>
2023-04-17 23:56:09 +0800

    map

---
+++ 第二阶段/chapter14/src/map/Map_.java
@@ -0,0 +1,15 @@
+package map_;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings({"all"})
+public class Map_ {
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put("no1", "hsp");
+        map.put("no2", "zwj");
+        map.put("no2", "zwj");
+        System.out.println(map);
+    }
+}
